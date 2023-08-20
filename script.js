89ce6fbc37fcd14f299395c1de19f8db166a1ea5
islam heddi <islamheddi19@gmail.com>
2023-08-20 18:46:26 +0100

    fixing the menu

---
+++ script.js
@@ -0,0 +1,16 @@
+var buttn = document.getElementById("mybtn");
+
+buttn.addEventListener("click", menu());
+
+function menu(){
+    var is_opened = false;
+    var menu_btn = document.getElementById("btn");
+    var navigation = document.querySelector("nav");
+    if(is_opened){
+        navigation.style.display = "none";
+        menu_btn.src = "./images/icon-hamburger.svg";    
+    }else{
+        navigation.style.display = "block";
+        menu_btn.src = "./images/icon-close.svg";
+    }
+}
