be9baaf16ddbaf67ad6f68117477294abc4e4742
islam heddi <islamheddi19@gmail.com>
2023-08-21 11:28:16 +0100

    i've fixed the menu script

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
-
+var is_opened = false;
 function menu(){
-    var is_opened = false;
+    
     var menu_btn = document.getElementById("btn");
     var navigation = document.querySelector("nav");
     if(is_opened){
+        is_opened = !is_opened;
         navigation.style.display = "none";
         menu_btn.src = "./images/icon-hamburger.svg";    
     }else{
+        is_opened = !is_opened;
         navigation.style.display = "block";
         menu_btn.src = "./images/icon-close.svg";
     }
